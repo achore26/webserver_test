00bf39f450ff7e138965a02356cbd55b40380751
Alby <achore26@gmail.com>
2024-11-07 11:36:24 +0300

    Changed the port back to 3000

--- index-ws.js
+++ index-ws.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
 });
 
-server.listen(3001, function () {
+server.listen(3000, function () {
   console.log('Server is listening on port 3000');
 });
 
